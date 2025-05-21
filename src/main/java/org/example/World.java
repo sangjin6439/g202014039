af419f02d65c22dc99766082dda52bd957b98762
sangjin <insangjin6439@gmail.com>
2025-05-21 09:20:08 +0900

    five

--- src/main/java/org/example/World.java
+++ src/main/java/org/example/World.java
@@ -5,4 +5,8 @@ public class World {
     public static void run() {
         System.out.println("202014039");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
